9785ce9de47154c5c29ee2560d751fa8aae26c1d
Yuval Adam <_@yuv.al>
2019-06-08 17:30:02 +0300

    Add clap dependency and basic CLI behavior

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+extern crate clap;
+
+use clap::{Arg, App};
+
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new("rustradio")
+        .version("0.1.0")
+        .author("Yuval Adam <_@yuv.al>")
+        .about("RustRadio SDR framework")
+        .get_matches();
+
+    println!("\n::: Starting RustRadio");
 }
